4e6078f7a3db5aca5b56f5ead7da63a9ac67a3d0
Joseph-01 <58695986+Joseph-01@users.noreply.github.com>
2022-04-26 17:10:20 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ food-palace/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+    <div className='foot'>
+      Copyright &copy; 2022| Powered by <b className='food'>Food Palace</b> 
+    </div>
+
+    </footer>
+  )
+}
+
+export default Footer
